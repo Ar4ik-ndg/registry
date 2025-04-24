20a524b7d46c29ce7078791485dae21ad5d3f678
agniaendie <endienasg2@gmail.com>
2025-04-25 00:16:50 +0500

    setup of modal component (localBase)

---
+++ web/app/components/modal-order/modal-order.tsx
@@ -0,0 +1,33 @@
+// импортируем css
+import './modal-order.css'
+
+type ModalOrderProps = {
+    showModal: boolean
+    showModalHandler: any //function (doChangeStateOfModal:bool) => void
+    date: string
+    status: string
+    doctor: any // TODO: заменить на тип из domain/models
+}
+
+// ModalOrder - Модальное окно записи
+export function ModalOrder(props:ModalOrderProps){
+    // React mojet vernut' tolko 1 result from function
+    if (props.showModal){
+        return (
+            <>
+                {/*используем css*/}
+                <div className={"red"}>
+                    {/* вызов хука через анонимную функцию т.к это ссылка*/}
+                    <span onClick={()=>props.showModalHandler(false)}>Закрыть</span>
+                    Приём <h1>{props.date}</h1>
+                    <p>Статус: {props.status}</p>
+                    <p>Специалист: {props.doctor.name}</p>
+                </div>
+            </>
+        )
+    }else{
+        return (
+            <></>
+        )
+    }
+}
