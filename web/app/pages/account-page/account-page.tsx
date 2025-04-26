08704a2d0c6545607d06333a5bc14bd0254ecf8b
Ar4ikoffc <artem.dln@mail.ru>
2025-04-27 03:01:27 +0500

    min account page without data

--- web/app/pages/account-page/account-page.tsx
+++ web/app/pages/account-page/account-page.tsx
@@ -1,5 +1,6 @@
 import "./account-page.css"
 import type { Route } from "./+types/account-page"
+import type { User } from "~/core/models"
 import {useState} from "react";
 
 export async function loader({params}: Route.LoaderArgs) {
@@ -10,7 +11,50 @@ export default function AccountPage({params}: Route.ComponentProps) {
     const name = params.userId
     return (
         <>
-            <h1>{name}</h1>
+            <main>
+                <h2>Ваши приемы</h2>
+                <div className="appointments">
+                </div>
+                <h2 className="user-info">Личная информация</h2>
+                <div className="user-info">
+                    <ul>
+                        <li className={"fullName "}>
+                            <p>ФИО</p>
+                            <input readOnly={true}/>
+                        </li>
+                        <li className={"birthday "}>
+                            <p>Дата рождения</p>
+                            <input readOnly={true} type="date"/>
+                        </li>
+                        <li className={"passport "}>
+                            <p>Серия, номер паспорта</p>
+                            <input readOnly={true} maxLength={10}/>
+                        </li>
+                        <li className={"snils "}>
+                            <p>СНИЛС</p>
+                            <input readOnly={true} maxLength={11}/>
+                        </li>
+                        <li className={"medPolicy "}>
+                            <p>Номер мед. полиса</p>
+                            <input readOnly={true} maxLength={16}/>
+                        </li>
+                        <li className={"email "}>
+                            <p>Email</p>
+                            <input readOnly={true} type="email"/>
+                        </li>
+                        <li className={"phone "}>
+                            <p>Номер телефона</p>
+                            <input readOnly={true} type="tel"/>
+                        </li>
+                    </ul>
+                    <div className="buttons">
+                        <div className="change-button">Изменить данные</div>
+                        <div className="change-button">Изменить пароль</div>
+                        <div className="cancel-button">Выйти</div>
+                    </div>
+
+                </div>
+            </main>
         </>
     )
 }
