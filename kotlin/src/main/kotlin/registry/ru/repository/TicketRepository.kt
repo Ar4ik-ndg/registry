d3b11c70bff826b0a6b4948099fbd22283571f6d
Ar4ikoffc <artem.dln@mail.ru>
2025-05-03 18:45:05 +0500

    Ticket.doctor: String -> Staff

--- kotlin/src/main/kotlin/registry/ru/repository/TicketRepository.kt
+++ kotlin/src/main/kotlin/registry/ru/repository/TicketRepository.kt
@@ -4,6 +4,7 @@ import registry.ru.model.Ticket
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import registry.ru.model.Staff
 import java.time.LocalDateTime
 
 @Repository
@@ -13,6 +14,6 @@ interface TicketRepository: JpaRepository<Ticket, String> {
     fun findByUserId(userId: String): List<Ticket>
     fun findByStatus(status: String): List<Ticket>
     fun findByDoctor(doctor: String): List<Ticket>
-    @Query("SELECT t.date FROM Ticket t WHERE t.date BETWEEN :start AND :end AND t.status NOT IN (:status)")
-    fun findByDateBetween(start: LocalDateTime, end: LocalDateTime, status: List<String>): List<LocalDateTime>?
+    @Query("SELECT t.date FROM Ticket t WHERE t.date BETWEEN :start AND :end AND t.status NOT IN (:status) AND t.doctor = :doctor")
+    fun findByDateBetweenAndStatusAndDoctor(start: LocalDateTime, end: LocalDateTime, status: List<String>, doctor: String): List<LocalDateTime>?
 }
